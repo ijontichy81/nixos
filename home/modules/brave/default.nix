17dd39a1764ea11b22d0ad1588dc6c0745c373d0
ijontichy81 <jujutsu420@gmail.com>
2026-04-28 20:47:04 +0200

    brave: add xdg-desktop-portal for file chooser

--- home/modules/brave/default.nix
+++ home/modules/brave/default.nix
@@ -2,9 +2,9 @@
   home.packages = [
     (pkgs.writeScriptBin "brave" ''
       #!${lib.getExe pkgs.bash}
-      export LD_LIBRARY_PATH="${pkgs.wayland}/lib:${pkgs.mesa}/lib:${pkgs.libGL}/lib:${pkgs.gtk3}/lib:$LD_LIBRARY_PATH"
+      export LD_LIBRARY_PATH="${pkgs.wayland}/lib:${pkgs.mesa}/lib:${pkgs.libGL}/lib:${pkgs.gtk3}/lib:${pkgs.xdg-desktop-portal}/lib:$LD_LIBRARY_PATH"
       export GSETTINGS_SCHEMA_DIR="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas:${pkgs.gtk3}/share/gsettings-schemas"
-      export XDG_DATA_DIRS="${pkgs.gsettings-desktop-schemas}/share:${pkgs.gtk3}/share:$XDG_DATA_DIRS"
+      export XDG_DATA_DIRS="${pkgs.gsettings-desktop-schemas}/share:${pkgs.gtk3}/share:${pkgs.xdg-desktop-portal}/share:$XDG_DATA_DIRS"
       exec -a "$0" "${pkgs.brave}/bin/.brave-wrapped" --ozone-platform=wayland "$@"
     '')
     pkgs.wayland
@@ -12,6 +12,7 @@
     pkgs.libGL
     pkgs.gsettings-desktop-schemas
     pkgs.gtk3
+    pkgs.xdg-desktop-portal
   ];
 
   xdg.mimeApps.defaultApplications = {
