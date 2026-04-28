4e9823a60ab99f3b19d23836fbb1e77b253d7f7a
ijontichy81 <jujutsu420@gmail.com>
2026-04-28 22:01:08 +0200

    brave: add 3 extensions (Enhancer for YouTube, Return YouTube Dislike, Vimium)

--- home/modules/brave/default.nix
+++ home/modules/brave/default.nix
@@ -1,4 +1,14 @@
 { config, pkgs, lib, ... }: {
+  programs.chromium = {
+    enable = true;
+    package = pkgs.brave;
+    extensions = [
+      "ponfpcnoihfmfllpaingbgckeeldkhle;https://clients2.google.com/service/update2/crx"
+      "gebbhagfogifgggkldgodflihgfeippi;https://clients2.google.com/service/update2/crx"
+      "dbepggeogbaibhgnhhndojpepiihcmeb;https://clients2.google.com/service/update2/crx"
+    ];
+  };
+
   xdg.mimeApps.defaultApplications = {
     "text/html" = "brave.desktop";
     "x-scheme-handler/http" = "brave.desktop";
