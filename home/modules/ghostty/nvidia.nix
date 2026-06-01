d9e65e303290deb5586b3670228a75cabc2a9fde
ijontichy81 <jujutsu420@gmail.com>
2026-06-01 10:27:09 +0200

    added cups avahi + print-gui

--- home/modules/ghostty/nvidia.nix
+++ home/modules/ghostty/nvidia.nix
@@ -5,7 +5,7 @@
     enable = true;
 
     settings = {
-      theme = "noctalia";
+      theme = "catppuccin mocha";
       window-padding-x = "8,4";
       window-padding-y = "12,4";
       font-family = "FiraCode Nerd Font";
