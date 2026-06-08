95294bec9a168387217f4fd9d6a1b9cf6e2ee4b6
IjonTichy81 <jujutsu420@hmail.com>
2026-06-08 16:48:58 +0200

    stable-diffusion-cpp-rocm setup

--- modules/comfyui/default.nix
+++ modules/comfyui/default.nix
@@ -5,6 +5,10 @@
 
   networking.firewall.allowedTCPPorts = [ 1234 ];
 
+  systemd.tmpfiles.rules = [
+    "d /home/marco/sd-cpp-data 0750 marco vip - -"
+  ];
+
   systemd.services.sd-server = {
     description = "stable-diffusion.cpp web UI server";
     after = [ "network-online.target" ];
@@ -16,7 +20,7 @@
       Type = "simple";
       User = "marco";
       Group = "vip";
-      ExecStart = "${pkgs.stable-diffusion-cpp-rocm}/bin/sd-server --listen-ip 127.0.0.1 --listen-port 1234";
+      ExecStart = "${pkgs.stable-diffusion-cpp-rocm}/bin/sd-server --listen-ip 127.0.0.1 --listen-port 1234 -m /home/marco/ComfyUI/models/checkpoints/illu/animosity_illustriousV11.safetensors";
       Restart = "on-failure";
       RestartSec = 5;
     };
