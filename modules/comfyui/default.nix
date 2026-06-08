42060dc8298b4436c267ddadea12924c7e514ee0
IjonTichy81 <jujutsu420@hmail.com>
2026-06-08 16:25:47 +0200

    switched comfyui with stable-diffusion-cpp-rocm for compatibility

--- modules/comfyui/default.nix
+++ modules/comfyui/default.nix
@@ -1,10 +1,28 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    comfy-ui
-    comfy-cli
-  ];
+  environment.systemPackages = [ pkgs.stable-diffusion-cpp-rocm ];
 
-  networking.firewall.allowedTCPPorts = [ 8188 ];
+  networking.firewall.allowedTCPPorts = [ 1234 ];
+
+  systemd.services.sd-server = {
+    description = "stable-diffusion.cpp web UI server";
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    wantedBy = lib.mkForce [ ];
+    path = [ pkgs.stable-diffusion-cpp-rocm ];
+
+    serviceConfig = {
+      Type = "simple";
+      User = "marco";
+      Group = "vip";
+      ExecStart = "${pkgs.stable-diffusion-cpp-rocm}/bin/sd-server --listen-ip 127.0.0.1 --listen-port 1234";
+      Restart = "on-failure";
+      RestartSec = 5;
+    };
+
+    environment = {
+      ROC_ENABLE_PRE_VEGA = "1";
+    };
+  };
 }
