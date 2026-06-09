f563804658bcba3219d183d58d53ee55f9a555b0
IjonTichy81 <jujutsu420@hmail.com>
2026-06-09 03:42:45 +0200

    fixed koboldcpp

--- modules/comfyui/default.nix
+++ modules/comfyui/default.nix
@@ -1,7 +1,15 @@
 { config, lib, pkgs, ... }:
 
+let
+  koboldcpp-wrapped = pkgs.writeShellScriptBin "koboldcpp" ''
+    RUNDIR=$(mktemp -d)
+    ln -sfn "${pkgs.koboldcpp}/bin" "$RUNDIR/embd_res"
+    ln -sfn "${pkgs.koboldcpp}/bin/koboldcpp.unwrapped" "$RUNDIR/koboldcpp"
+    exec "$RUNDIR/koboldcpp" "$@"
+  '';
+in
 {
-  environment.systemPackages = [ pkgs.koboldcpp ];
+  environment.systemPackages = [ koboldcpp-wrapped ];
 
   networking.firewall.allowedTCPPorts = [ 5001 ];
 
@@ -15,7 +23,7 @@
       Type = "simple";
       User = "marco";
       Group = "vip";
-      ExecStart = "${pkgs.koboldcpp}/bin/koboldcpp --usevulkan --host 127.0.0.1 --port 5001 --sdmodel /home/marco/ComfyUI/models/checkpoints/illu/animosity_illustriousV11.safetensors --sdflashattention --sdoffloadcpu";
+      ExecStart = "${koboldcpp-wrapped}/bin/koboldcpp --usevulkan --host 127.0.0.1 --port 5001 --sdmodel /home/marco/ComfyUI/models/checkpoints/illu/animosity_illustriousV11.safetensors --sdflashattention --sdoffloadcpu";
       Restart = "on-failure";
       RestartSec = 5;
     };
